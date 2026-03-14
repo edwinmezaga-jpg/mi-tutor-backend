3d3e6ebd7b9214aaabef3bc20847231dbb862da8
edwinmezaga-jpg <edwinmezaga@gmail.com>
2026-03-13 19:33:23 -0700

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo "📦 Instalando yt-dlp..."
-curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /opt/render/project/src/yt-dlp
-chmod +x /opt/render/project/src/yt-dlp
-echo "✅ yt-dlp instalado correctamente"
-npm install
